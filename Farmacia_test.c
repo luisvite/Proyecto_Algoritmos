604e4a846934da9f8e990074d2087f6313eee539
Luis Enrique Vite Aquino <luis.vite0307@gmail.com>
2016-03-08 23:08:15 -0600

    ...

--- Farmacia_test.c
+++ Farmacia_test.c
@@ -3,15 +3,31 @@
 #define MAX 30
 /*Este documento es sin errores de compilacion nada mas es para ir testeando que todo valla bien hasta que  terminemos el proyecto*/
 int indice;
+typedef struct
+{	
+	char nombre_med[MAX];
+	char grupo_med[MAX];
+	float precio;
+	char trata[MAX];
+	char via_administracion[MAX];
+}Medicamento;
 
 int menu(char texto[], int n);
 
 int main(){
-	int opcion;
-		
+	int num,o1,o2;
+	Medicamento *med;
+	
+	printf("Cuantos medicamentos quiere registrar como maximo: ");
+	scanf("%d",&num);
+	
+	med=(Medicamento *)malloc(num*sizeof(Medicamento));
+	
+	printf("\n");
+	
 	do{
-		opcion = Menu("\t\t\tMENU PRINCIPAL\n1) Agregar\n2) Borrar\n3) Consultar\n4) Actualizar\n5) Salir\n\nSeleccione una opcion: ",5);
-		switch(opcion)
+		o1 = Menu("\t\t\tMENU PRINCIPAL\n1) Agregar\n2) Borrar\n3) Consultar\n4) Actualizar\n5) Salir\n\nSeleccione una opcion: ",5);
+		switch(o1)
 		{
 			case 1:
 				break;
@@ -22,7 +38,7 @@ int main(){
 			case 4:
 				break;
 		}
-	}while(opcion!=5);
+	}while(o1!=5);
 }
 
 int Menu(char texto[], int n)
